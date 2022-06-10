b2b0ecfa3fb4377f9e81a5575110a1c658be1869
kkr629 <kkr00629@gmail.com>
2022-06-11 00:49:08 +0900

    recaptcha

--- app/src/main/java/com/inhatc/contact_messenger/MainActivity.java
+++ app/src/main/java/com/inhatc/contact_messenger/MainActivity.java
@@ -1,16 +1,24 @@
 package com.inhatc.contact_messenger;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.safetynet.SafetyNet;
+import com.google.android.gms.safetynet.SafetyNetApi;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.DatabaseError;
@@ -18,7 +26,16 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.ValueEventListener;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,GoogleApiClient.ConnectionCallbacks {
+    //Initialize Variable
+    //변수 초기화
+
+    CheckBox checkBox;
+    GoogleApiClient googleApiClient;
+
+    //Put SiteKey as a String
+    //recaptcha 사이트 키를 String으로 정의
+    String SiteKey = "6Lcxh10gAAAAAKUvRrPpy1IOQJ-bcAIK3wAMH_tH";
 
     FirebaseDatabase myFirebase;
     DatabaseReference myDB_Reference=null;
@@ -54,8 +71,54 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         myFirebase = FirebaseDatabase.getInstance();
         myDB_Reference = myFirebase.getReference();
+
+        //Assign Variable
+        //승인 변수
+        checkBox = findViewById(R.id.check_box);
+
+        //Create Google Api Client
+        //Api 생성
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(SafetyNet.API)
+                .addConnectionCallbacks(MainActivity.this)
+                .build();
+        googleApiClient.connect();
+
+        checkBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Set if condition when checkbox checked
+                if (checkBox.isChecked()){
+                    SafetyNet.SafetyNetApi.verifyWithRecaptcha(googleApiClient,SiteKey)
+                            .setResultCallback(new ResultCallback<SafetyNetApi.RecaptchaTokenResult>() {
+                                @Override
+                                public void onResult(@NonNull SafetyNetApi.RecaptchaTokenResult recaptchaTokenResult) {
+                                    Status status = recaptchaTokenResult.getStatus();
+                                    if((status != null) && status.isSuccess()){
+                                        //Display Successful Message
+                                        Toast.makeText(getApplicationContext()
+                                                ,"Successfully Varified..."
+                                                ,Toast.LENGTH_SHORT).show();
+                                        checkBox.setTextColor(Color.GREEN);
+                                    }
+                                }
+                            });
+                }else{
+                    //Default Checkbox text color
+                    checkBox.setTextColor(Color.BLACK);
+                }
+            }
+        });
+    }
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
     }
 
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
     public void onClick(View v){
         if(v==btnLogin){
             strID =  edtID.getText().toString().trim();
@@ -90,6 +153,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 });
             }
         }
+
         else if(v== btnRegister){
             Intent registerIntent = new Intent(MainActivity.this, RegisterActivity.class);
             startActivity(registerIntent);
